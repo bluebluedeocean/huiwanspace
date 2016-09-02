0a7a27f4430e7f9697948e2b7de6b426e84231bc
ZMeng <1552579525@qq.com>
2016-09-02 22:14:29 +0800

    9-2 begin edit activities part
    开始开发活动页面，实现了活动fragment中三个fragment切换框架的实现

---
+++ Gonghui1.0/app/src/main/java/com/example/project/gonghui10/fragment/ActivityFragment02.java
@@ -0,0 +1,26 @@
+package com.example.project.gonghui10.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.project.gonghui10.R;
+
+
+public class ActivityFragment02 extends Fragment {
+
+
+
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.fragment_activity02, container, false);
+		return view;
+		
+	}
+	
+}
--- Gonghui1.0/app/src/main/java/com/example/project/gonghui10/fragment/CommunityFragment.java
+++ Gonghui1.0/app/src/main/java/com/example/project/gonghui10/fragment/CommunityFragment.java
@@ -1,39 +1,51 @@
 package com.example.project.gonghui10.fragment;
 
-import android.app.Fragment;
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.example.project.gonghui10.Config;
 import com.example.project.gonghui10.R;
-import com.example.project.gonghui10.activity.UserNickActivity;
-import com.example.project.gonghui10.net.textuserinfo;
-import com.example.project.gonghui10.util.RoundImageLoader;
 import com.example.project.gonghui10.view.TitleBarView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 
 public class CommunityFragment extends Fragment {
     private Context mContext;
     private View mBaseView;
+    private TitleBarView mTitleBarView;
+    private SwipeRefreshLayout refreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mContext = getActivity();
         mBaseView = inflater.inflate(R.layout.fragment_community, null);
-
+        findView();
+        init();
+        refresh();
         return mBaseView;
+    }
+
+    private void refresh() {
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                init();
+                refreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void findView() {
+        mTitleBarView = (TitleBarView) mBaseView.findViewById(R.id.title_bar);
+        refreshLayout = (SwipeRefreshLayout) mBaseView.findViewById(R.id.refresher);
+    }
+
+    private void init() {
+        mTitleBarView.setCommonTitle(View.GONE, View.VISIBLE, View.GONE, View.GONE);
+        mTitleBarView.setTitleText(R.string.community);
     }
 }
